ed8a432613a11ee81e0ce97d7ff43da17ae7cf8e
Greg Gibeling <gdgib@outlook.com>
2019-11-04 19:49:10 -0800

    G2-629 Unit tests (partial)

--- ha-metadata/src/test/java/com/g2forge/habitat/metadata/TestValueMetadata.java
+++ ha-metadata/src/test/java/com/g2forge/habitat/metadata/TestValueMetadata.java
@@ -1,18 +1,43 @@
 package com.g2forge.habitat.metadata;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import org.junit.Test;
 
 import com.g2forge.alexandria.test.HAssert;
-import com.g2forge.habitat.metadata.IMetadata;
-import com.g2forge.habitat.metadata.Metadata;
-import com.g2forge.habitat.metadata.TestAnnotationMetadata.Annotated;
-import com.g2forge.habitat.metadata.TestAnnotationMetadata.Retained;
 
 public class TestValueMetadata {
+	@A("A")
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface A {
+		public String value();
+	}
+
+	@A("B")
+	public class B {}
+
 	@Test
 	public void meta() {
 		final IMetadata metadata = Metadata.getStandard();
-		final Retained retained = metadata.of(Annotated.class).get(Retained.class);
-		HAssert.assertException(Throwable.class, () -> metadata.of(retained));
+		final A aOnB = metadata.of(B.class).get(A.class);
+		HAssert.assertEquals("B", aOnB.value());
+		final A aOnA = metadata.of(aOnB).get(A.class);
+		HAssert.assertEquals("A", aOnB.value());
+	}
+
+	@Test
+	public void override() {
+
+	}
+
+	@Test
+	public void value() {
+
+	}
+
+	@Test
+	public void element() {
+
 	}
 }
